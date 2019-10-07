4b1f0b99a387a6a68e6a087fec9bb2d1c5b2d079
Carol Hamer <carolhamer@Dybusters-Mac-mini.local>
2019-10-07 16:41:12 +0200

    code cleanup

--- updateServer.py
+++ updateServer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
 from urllib.request import urlopen
 import json
 import os
@@ -8,13 +9,6 @@ from os.path import join, isfile
 import subprocess
 from shutil import copy, move
 import stat
-
-response = urlopen('https://launchermeta.mojang.com/mc/game/version_manifest.json')
-latest_manifest = json.load(response)
-latest_version = latest_manifest["latest"]["release"]
-print('latest version %s' % latest_version)
-latest_file = 'minecraft_server.%s.jar' % (latest_version)
-docker_path = 'minecraft/minecraft_server.jar'
 
 def get_download_url(latest_manifest, latest_version):
     for version in latest_manifest['versions']:
@@ -24,22 +18,35 @@ def get_download_url(latest_manifest, latest_version):
             return meta_json['downloads']['server']['url']
 
 
+def build_if_changed(latest_version):
+    latest_file = 'minecraft_server.%s.jar' % (latest_version)
+    if isfile(latest_file):
+        print('file exists')
+    else:
+        server_url = get_download_url(latest_manifest, latest_version)
+        print('downloading: %s from %s\n' % (latest_file, server_url))
+        CHUNK = 16 * 1024
+        jar_data = urlopen(server_url)
+        with open(latest_file, 'wb') as outfile:
+            while True:
+                chunk = jar_data.read(CHUNK)
+                if not chunk:
+                    break
+                outfile.write(chunk)
+        server_in_image = 'minecraft/minecraft_server.jar'
+        copy(latest_file, server_in_image)
+        st = os.stat(server_in_image)
+        os.chmod(server_in_image, st.st_mode | stat.S_IEXEC)
+        subprocess.call(["docker", "build", "-t", "minecraft:%s" % latest_version, "minecraft"])
 
-if isfile(latest_file):
-    print('file exists')
-else:
-    server_url = get_download_url(latest_manifest, latest_version)
-    print('downloading: %s from %s\n' % (latest_file, server_url))
-    CHUNK = 16 * 1024
-    jar_data = urlopen(server_url)
-    with open(latest_file, 'wb') as outfile:
-        while True:
-            chunk = jar_data.read(CHUNK)
-            if not chunk: break
-            outfile.write(chunk)
+def main(argv):
+    response = urlopen('https://launchermeta.mojang.com/mc/game/version_manifest.json')
+    latest_manifest = json.load(response)
+    latest_version = latest_manifest["latest"]["release"]
+    print('latest version %s' % latest_version)
+    build_if_changed(latest_version)
 
-copy(latest_file, docker_path)
-st = os.stat(docker_path)
-os.chmod(docker_path, st.st_mode | stat.S_IEXEC)
+if __name__ == "__main__":
+    __file__ = sys.argv[0]
+    sys.exit( main(sys.argv) )
 
-subprocess.call(["docker", "build", "-t", "minecraft:%s" % latest_version, "minecraft"])
